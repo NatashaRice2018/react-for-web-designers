f096b66c7acb2f16790d01f443bb0e9fce42d8e7
Natasha Rice <natasharice2014@gmail.com>
2018-04-29 21:27:43 +0000

    Installing React in an existing site

--- Exercise Files/Ch02/wip/start/app.js
+++ Exercise Files/Ch02/wip/start/app.js
@@ -1,6 +1,8 @@
 (function() {
-  "use strict";
-
+  "use strict"; //helps us avoid errors
+  //IFFY - Immedeatly called. 
   // Start here
+  var ProductCustomizer = React.createElement('div', { className: "customizer" }, "Product Customizer will go here");
 
+  ReactDOM.render(ProductCustomizer, document.getElementById('react-root'));
 })();
