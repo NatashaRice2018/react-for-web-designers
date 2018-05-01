39b7ed0659655626ac328f760d16afeb2fcfa0fc
Natasha Rice <natasharice2014@gmail.com>
2018-05-01 20:24:25 +0000

    Set up first simple component

--- Exercise Files/Ch02/wip/start/app.js
+++ Exercise Files/Ch02/wip/start/app.js
@@ -2,7 +2,25 @@
   "use strict"; //helps us avoid errors
   //IFFY - Immedeatly called. 
   // Start here
-  var ProductCustomizer = React.createElement('div', { className: "customizer" }, "Product Customizer will go here");
 
-  ReactDOM.render(ProductCustomizer, document.getElementById('react-root'));
+  function ProductImage(props) {
+
+    return React.createElement(
+      "img", {
+        src: "../../../assets/red.jpg",
+        alt: "Product Image"
+      });
+  }
+
+  function ProductCustomizer(props) {
+    return React.createElement(
+      "div", { className: "customizer" },
+      React.createElement("div", { className: "product-image" },
+        React.createElement(ProductImage)
+        )
+    );
+  }
+
+  ReactDOM.render(React.createElement(ProductCustomizer),
+  document.getElementById('react-root'));
 })();
