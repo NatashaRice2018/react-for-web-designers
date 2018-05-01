30dcc290025068d70f6c87970bdc418d26e4e1a0
Natasha Rice <natasharice2014@gmail.com>
2018-05-01 22:32:56 +0000

    Start using JSX with Babel Help

--- Exercise Files/Ch02/wip/start/app.js
+++ Exercise Files/Ch02/wip/start/app.js
@@ -3,24 +3,23 @@
   //IFFY - Immedeatly called. 
   // Start here
 
-  function ProductImage(props) {
 
-    return React.createElement(
-      "img", {
-        src: "../../../assets/red.jpg",
-        alt: "Product Image"
-      });
+
+  function ProductImage(props) {
+    return <img src= "../../../assets/red.jpg" alt= "Product Image"/>;
   }
 
   function ProductCustomizer(props) {
-    return React.createElement(
-      "div", { className: "customizer" },
-      React.createElement("div", { className: "product-image" },
-        React.createElement(ProductImage)
-        )
+
+    return (
+      <div className="customizer">
+        <div className="product-image">
+        <ProductImage/>
+      </div>
+      </div>
     );
   }
 
-  ReactDOM.render(React.createElement(ProductCustomizer),
-  document.getElementById('react-root'));
+  ReactDOM.render(<ProductCustomizer/>,
+    document.getElementById('react-root'));
 })();
