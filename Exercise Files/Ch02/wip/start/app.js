a8441acb13a6996afce7bbd122012f8db4f13d73
Natasha Rice <natasharice2014@gmail.com>
2018-05-04 01:53:23 +0000

    add color selector

--- Exercise Files/Ch02/wip/start/app.js
+++ Exercise Files/Ch02/wip/start/app.js
@@ -25,6 +25,27 @@
     );
   }
 
+function ColorSelector(props) {
+    function colorOptions() {
+      return props.colors.map(function(name) {
+        return (
+          <option value={name} key={name}>
+            {name}
+          </option>
+        );
+      });
+    }
+
+    return (
+      <div className="field-group">
+        <label htmlFor="color-options">Color:</label>
+        <select defaultValue={props.color} name="colorOptions" id="color-options">
+          {colorOptions()}
+        </select>
+      </div>
+    );
+  }
+
 
   function ProductImage(props) {
     return (<img src={`../../../assets/${props.color}.jpg`} alt="Product Image" />);
@@ -33,8 +54,10 @@
   var ProductCustomizer = createReactClass({
     getInitialState: function() {
       var sizes = window.Inventory.allSizes;
+      var colors = window.Inventory.allColors;
       return {
         color: "red",
+        colors: colors,
         size: 8,
         sizes: sizes
       };
@@ -50,6 +73,7 @@
       
       	<div className="selectors">
       	<SizeSelector size={this.state.size} sizes={this.state.sizes}/> 
+      	<ColorSelector color={this.state.color} colors={this.state.colors} />
       	</div>
       </div>
       );
